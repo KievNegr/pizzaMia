20f8d5588739eaa286c10f4391f5f6d10de49a2d
Vlad <hissone@gmail.com>
2019-10-10 15:38:59 +0300

    Update 1main-local.php

--- common/config/1main-local.php
+++ common/config/1main-local.php
@@ -5,7 +5,7 @@ return [
             'class' => 'yii\db\Connection',
             'dsn' => 'mysql:host=localhost;dbname=pizza',
             'username' => 'pizza',
-            'password' => 'kjfij38udf',
+            'password' => 'dadsd',
             'charset' => 'utf8',
         ],
         'mailer' => [
